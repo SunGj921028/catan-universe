3fe5d859252cce63c447e61499e48853b1abf166
NingQung <yorha21s@gmail.com>
2023-06-12 16:26:04 +0800

    map_v2

--- Final Project ver1/map.h
+++ Final Project ver1/map.h
@@ -3,10 +3,14 @@
 
 int8_t map[23][13][5];
 
-void map_point_init(int8_t i,int8_t j,int8_t map_p_type);
-
-
+void d3_pd(int32_t x,int32_t y);
 
 void map_init();
 
-int32_t map_print();
+int32_t map_print(int8_t printer_mood);
+
+int32_t build_village(int32_t player_ID, int32_t point_ID, int8_t is_init);
+
+int32_t build_road(int32_t player_ID, int32_t road_ID);
+
+int32_t harvest(int32_t dice_number,int32_t *harvest_resource);
